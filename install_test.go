7a4f2d95c8112bbdc40ff59235c9aec720524fc3
MURAOKA Taro <koron.kaoriya@gmail.com>
2022-09-23 23:34:29 +0900

    test install tar.gz archive

--- install_test.go
+++ install_test.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"flag"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
-func TestInstall(t *testing.T) {
+func TestInstallZip(t *testing.T) {
+	root := t.TempDir()
 	got := testSubcmd(t, nil, func() {
-		root := t.TempDir()
 		fs := flag.NewFlagSet("install", flag.ContinueOnError)
 		err := install(fs, []string{"-root", root, "-goos", "windows", "-goarch", "amd64", "go1.18.6"})
 		if err != nil {
@@ -15,4 +17,38 @@ func TestInstall(t *testing.T) {
 		}
 	})
 	assertStdout(t, "", got)
+
+	// check installed Go directory
+	godir := filepath.Join(root, "go1.18.6.windows-amd64")
+	fi, err := os.Stat(godir)
+	if err != nil {
+		t.Errorf("failed to stat: %s", err)
+		return
+	}
+	if !fi.IsDir() {
+		t.Errorf("not found install directory: %s", godir)
+	}
+}
+
+func TestInstallTarGz(t *testing.T) {
+	root := t.TempDir()
+	got := testSubcmd(t, nil, func() {
+		fs := flag.NewFlagSet("install", flag.ContinueOnError)
+		err := install(fs, []string{"-root", root, "-goos", "linux", "-goarch", "amd64", "go1.18.6"})
+		if err != nil {
+			t.Errorf("install failed: %s", err)
+		}
+	})
+	assertStdout(t, "", got)
+
+	// check installed Go directory
+	godir := filepath.Join(root, "go1.18.6.linux-amd64")
+	fi, err := os.Stat(godir)
+	if err != nil {
+		t.Errorf("failed to stat: %s", err)
+		return
+	}
+	if !fi.IsDir() {
+		t.Errorf("not found install directory: %s", godir)
+	}
 }
